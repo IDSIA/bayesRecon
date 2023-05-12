accd7cb39ebef58866121bd0d3f450bd8d17e4ee
Dario Azzimonti <dario.azzimonti@gmail.com>
2023-05-12 17:02:26 +0200

    First documentation for package help

--- R/bayesReco-package.R
+++ R/bayesReco-package.R
@@ -1,3 +1,10 @@
+#' @section Main functions:
+#'
+#' The package implements reconciliation via conditioning for probabilistic forecasts of hierarchical time series. The main functions are
+#'
+#' * [reconc_IS]: a generic tool for reconciliation of any probabilistic time series forecast via importance sampling;
+#' * [reconc_gaussian]: implements analytic formulae for the reconciliation of Gaussian base forecasts.
+#'
 #' @keywords internal
 "_PACKAGE"
 
