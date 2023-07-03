b45a998f032bbf98686a94b9e988c2c4ee2865f1
nicorbtt <iamnicown@gmail.com>
2023-07-03 16:21:17 +0200

    vignette: example 2 and 3

---
+++ R/M3example.R
@@ -0,0 +1,13 @@
+#' A time series from the M3 forecasting competition
+#'
+#' A monthly time series, from the M3 forecasting competition ("N1402").
+#'
+#' @format
+#' List of time series of class \link[stats]{ts}.
+#' 
+#' @usage 
+#' train <- M3example$train
+#' test <- M3example$test
+#' 
+#' @source [https://forecasters.org/resources/time-series-data/m3-competition/](https://forecasters.org/resources/time-series-data/m3-competition/)
+"M3example"
--- R/carpart.R
+++ R/carpart.R
@@ -3,7 +3,7 @@
 #' A monthly time series from the `carparts` dataset, 51 observations, Jan 1998 - Mar 2002.
 #'
 #' @format
-#' Univariate ime series of class \link[stats]{ts}.
+#' Univariate time series of class \link[stats]{ts}.
 #' 
 #' @source Hyndman, R.J., Koehler, A.B., Ord, J.K., and Snyder, R.D., (2008) Forecasting with exponential 
 #' smoothing: the state space approach, Springer
